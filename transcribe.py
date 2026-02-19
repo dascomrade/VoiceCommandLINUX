c68f32e71e82d3827198db00badba188576a5181
dascomrade <DeComrade@proton.me>
2026-02-19 00:01:51 -0600

    Add files via upload
    not too much in it tbf

---
+++ transcribe.py
@@ -0,0 +1,62 @@
+import whisper
+import pyaudio
+import wave
+import subprocess
+
+chunk = 1024  # Record in chunks of 1024 samples
+sample_format = pyaudio.paInt16  # 16 bits per sample
+channels = 2
+fs = 44100  # Record at 44100 samples per second
+seconds = 3
+filename = "output.wav"
+
+p = pyaudio.PyAudio()  # Create an interface to PortAudio
+
+print('Recording')
+
+stream = p.open(format=sample_format,
+                channels=channels,
+                rate=fs,
+                frames_per_buffer=chunk,
+                input=True)
+
+frames = []  # Initialize array to store frames
+
+# Store data in chunks for 3 seconds
+for i in range(0, int(fs / chunk * seconds)):
+    data = stream.read(chunk)
+    frames.append(data)
+
+# Stop and close the stream 
+stream.stop_stream()
+stream.close()
+# Terminate the PortAudio interface
+p.terminate()
+
+print('Finished recording')
+
+# Save the recorded data as a WAV file
+wf = wave.open(filename, 'wb')
+wf.setnchannels(channels)
+wf.setsampwidth(p.get_sample_size(sample_format))
+wf.setframerate(fs)
+wf.writeframes(b''.join(frames))
+wf.close()
+
+model = whisper.load_model("base")
+result = model.transcribe("output.wav")
+with open('command.txt', 'w') as f:
+    f.write(result["text"])
+
+
+
+with open('command.txt') as f:
+    text = f.read()
+if 'discord' in text.lower():
+    subprocess.Popen('discord', shell=True)
+if 'steam' in text.lower():
+    subprocess.Popen('steam', shell=True)
+if 'pc' in text.lower():
+    subprocess.Popen('xdg-open https://pcpartpicker.com', shell=True)
+if 'Arch wiki' in text.lower():
+    subprocess.Popen('xdg-open https://wiki.archlinux.org/title/Main_page', shell=True)
